d5a90425e20877b8993a7347bf38e4c4abd80989
Pramod kumar <pramodkumarp6@gmail.com>
2022-03-19 03:30:19 +0530

    welcome

---
+++ app/src/main/java/com/user/transportuser/SplasActivity.kt
@@ -0,0 +1,11 @@
+package com.user.transportuser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
